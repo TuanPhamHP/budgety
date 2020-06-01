6630f8dc8f17c8d10be67a2bb1a8c0557b2c0aa3
TuanPhamHP <phamanhtuan290896@gmail.com>
2020-06-01 22:40:18 +0700

    removed pic

--- src/components/MoneyAction.js
+++ src/components/MoneyAction.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { statuses } from "../constants/constants";
-import sumBg from "../assets/image/savingmoney.jpg";
 // const newItem = {
 //     description : "",
 //     value:"",
